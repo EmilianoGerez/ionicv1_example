eb461057110188aab61de65235d7e4167d41d183
EmilianoGerez <emiliano.gerez@gmail.com>
2017-04-24 13:25:09 -0300

    login error msg

--- app/js/controllers/auth.controller.js
+++ app/js/controllers/auth.controller.js
@@ -1,36 +1,47 @@
-(function() {
+(function () {
 
   angular
     .module('App')
     .controller('AuthCtrl', AuthCtrl);
 
-  AuthCtrl.$inject = ['$scope', '$state', '$authService'];
+  AuthCtrl.$inject = ['$scope', '$state', '$authService', '$ionicPopup'];
 
-  function AuthCtrl($scope, $state, $authService) {
+  function AuthCtrl($scope, $state, $authService, $ionicPopup) {
     var vm = $scope;
     vm.spinner = false;
+    vm.loginError = "";
 
     vm.credentials = {
       dni: '',
       password: ''
-    }
+    };
+    vm.credentialsInitial = angular.copy(vm.credentials);
 
-    vm.login = function() {
+    vm.login = function () {
       vm.spinner = true;
-      $authService
-        .findUser(vm.credentials)
-        .then(function(response) {
+      $authService.findUser(vm.credentials)
+        .then(function (response) {
           vm.spinner = false;
-          $authService.setCurrentUser(response);
+          $authService.setCurrentUser(response.data);
           vm.$apply();
           $state.go('private.dashboard');
           // local storage
         })
-        .catch(function(error) {
+        .catch(function (error) {
           vm.spinner = false;
+
+          var alertPopup = $ionicPopup.alert({
+            title: 'DNI o Contraseña invalido',
+            template: 'Ingresa tus credenciales correctamente'
+          });
+
+          alertPopup.then(function (res) {
+            vm.credentials = angular.copy(vm.credentialsInitial);
+          });
+          vm.$apply();
           console.log(error);
         });
-    }
+    };
   }
 
 })();
--- app/js/services/auth.service.js
+++ app/js/services/auth.service.js
@@ -10,29 +10,34 @@
     var self = this;
     var currentUser = null;
 
-    self.findUser = function(crendetials) {
-      console.log(crendetials);
+    self.findUser = function(credentials) {
+      console.log(credentials);
       return new Promise(function(resolve, reject) {
+        if(credentials.dni !== 1234 || credentials.password !== "1234"){
+          reject();
+        }
         setTimeout(function() {
           resolve({
+            status: 200,
+            data: {
             firstname: "Emi",
             lastname: "Gerez",
             dni: "33439203",
             vehicles: [{
               deviceId: 1234
             }]
-          })
-        }, 1000)
+          }});
+        }, 1000);
       });
-    }
+    };
 
     self.getCurrentUser = function() {
       return self.currentUser;
-    }
+    };
 
     self.setCurrentUser = function(user) {
-      return self.currentUser = user;
-    }
+      return (self.currentUser = user);
+    };
   }
 
 })();
